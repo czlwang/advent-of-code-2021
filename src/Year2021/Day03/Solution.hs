f5ca24c859a44860607b7b2c0452dba5705b210f
Christopher Wang <czw@mit.edu>
2021-12-03 01:30:55 -0500

    final day 3

--- src/Year2021/Day03/Solution.hs
+++ src/Year2021/Day03/Solution.hs
@@ -5,10 +5,10 @@ solve :: IO()
 solve = do
             test <- readFile "/home/czw/Documents/2021/aoc2021/src/Year2021/Day03/test_input1.txt"
             input1 <- readFile "/home/czw/Documents/2021/aoc2021/src/Year2021/Day03/input1.txt"
-            print $ (solve1 . parse) test
             print $ 198 == (solve1 . parse) test
+            print $ (solve1 . parse) input1
             print $ 230 == (solve2 . parse) test
-            print $ (solve2 . parse) test
+            print $ (solve2 . parse) input1
               where parse fileLines = do
                                   line <- lines fileLines
                                   pure (map (\x -> read [x]) line)
@@ -28,7 +28,7 @@ solve1 bits = gamma*epsilon
               gamma = signed2int $ map signum votes
               epsilon = signed2int $ map (signum . negate) votes
 
-solve2 bits = trace (show o2 ++ " " ++ show co2) o2*co2
+solve2 bits = o2*co2
               where bitLength = (length . head) bits
                     reversed = map reverse bits
                     o2 = bits2int $ reverse $ head $ filterBits (bitLength-1) reversed "o2"
@@ -37,7 +37,7 @@ solve2 bits = trace (show o2 ++ " " ++ show co2) o2*co2
 filterBits :: Int -> [[Int]] -> String -> [[Int]]
 filterBits _ [x] _ = [x]
 filterBits (-1) bits _ = bits
-filterBits idx bits mode = trace (show mode ++ show bits ++ " " ++ show vote ++ " " ++ show filterBit ++ " " ++ " " ++ show bit ++ " " ++ show idx) filterBits (idx-1) filtered mode
+filterBits idx bits mode = filterBits (idx-1) filtered mode
                         where signed = map bits2signed bits
                               vote = sum [x | s <- signed, let x=s!!idx]
                               bit = signed2bit $ signum vote
