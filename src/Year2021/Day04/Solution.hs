d9d4400e58d98f2eb2eefa9938c1ffa1b453fc67
Christopher Wang <czw@mit.edu>
2021-12-04 23:06:23 -0500

    fixed parsing

--- src/Year2021/Day04/Solution.hs
+++ src/Year2021/Day04/Solution.hs
@@ -1,7 +1,10 @@
 module Year2021.Day04.Solution (solve) where
 import Text.ParserCombinators.Parsec
 import Debug.Trace
-
+import Data.Map (Map)
+--import Control.Lens
+import Control.Monad
+import Data.Array
 
 solve :: IO()
 solve = do
@@ -28,11 +31,15 @@ numLine = do
 
 boardLine :: GenParser Char st [Int]
 boardLine = do
-            cells <- spaces *> sepBy (many digit) (many1 (char ' '))
+            cells <- many (char ' ') *> sepBy1 (many1 digit) (many1 (char ' '))
             return $ map read cells
 
+list2array :: [[Int]] -> Array (Int, Int) Int
+list2array b = listArray ((0,0),(n, n)) (let y = [x | row <- b, x <- row] in trace (show y) y)
+                where n = length b - 1
+
 board :: GenParser Char st [[Int]]
-board = endBy boardLine eol
+board = endBy1 boardLine eol
 
 boards :: GenParser Char st [Board]
 boards = sepBy board eol
@@ -50,5 +57,20 @@ parseInput = parse day4 "(unknown)"
 
 solve1 :: Day4AInput -> Int
 solve1 input = 1
-                where bs = d4boards input 
+                where boardNums = d4boards input
+                      initBoard n = listArray (n,n) [1 | _ <- [0..n*n]]
+                      boardSpots = [initBoard (length (head boardNums)) | _ <- [0..length boardNums]]
                       nums = d4called input
+
+--callNum :: [Board] -> [Array Int Int] -> Int
+--callNum boards status n = 
+
+--findWinner :: [Board] -- ^ bingo boards
+--  -> [Array Int Int] -- ^ bingo boards call status
+--  -> [Int] -- ^ list of called nums
+--  -> Int -- solve1 answer
+--findWinner boards boardStatus (x:xs) = case winner of (Just winnerIdx) -> 0 -- answer winnerIdx
+--                                                      Nothing -> 0
+--                                        where newStatus winner = callNum boards boardStatus x
+--                                              --(boardWinner, statusWinner) = boards !! winnerIdx
+--                                              --answer n = x*(sum ([(newStatus !! i * boards !! i]
