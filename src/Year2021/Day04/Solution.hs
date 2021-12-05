954d4966500233df118ae1d3adf99fd4c9da6877
Christopher Wang <czw@mit.edu>
2021-12-05 02:03:18 -0500

    day 4 part 1

--- src/Year2021/Day04/Solution.hs
+++ src/Year2021/Day04/Solution.hs
@@ -11,13 +11,10 @@ import Data.Array
 solve :: IO()
 solve = do
             test <- readFile "/home/czw/Documents/2021/aoc2021/src/Year2021/Day04/test_input1.txt"
-            --input1 <- readFile "/home/czw/Documents/2021/aoc2021/src/Year2021/Day03/input1.txt"
+            input1 <- readFile "/home/czw/Documents/2021/aoc2021/src/Year2021/Day04/input1.txt"
             print $ parseInput test
-            print $ second solve1 $ parseInput test
-            --print $ 150 == (solve1 . parse) test
-            --print $ (solve1 . parse) input1
-            --print $ 900 == (solve2 (0,0,0) . parse) test
-            --print $ (solve2 (0,0,0) . parse) input1
+            print $ second ((4512==).solve1) $ parseInput test
+            print $ second solve1 $ parseInput input1
 
 type Board = Array (Int, Int) Int
 type Status = Array (Int, Int) Int
@@ -71,7 +68,7 @@ solve1 input = findWinner boardNums boardSpots nums vmaps
                       vmaps = map arrayValueMap boardNums
 
 updateStatus :: Int -> ValueMap -> Board -> Board
-updateStatus n vmap status = case M.lookup n vmap of Just (i,j) -> trace (show "i " ++ show i ++ " j" ++ show j)status // [((i,j),1)]
+updateStatus n vmap status = case M.lookup n vmap of Just (i,j) -> status // [((i,j),1)]
                                                      Nothing -> status
 
 callNum :: [Board] -> [Board] -> [ValueMap] -> Int -> [Board]
@@ -81,30 +78,28 @@ arrayValueMap :: Board -> ValueMap
 arrayValueMap a = M.fromList $ map (\(x,y) -> (y,x)) $ assocs a
 
 checkBoardWin :: Board -> Bool
-checkBoardWin b = trace (show rowWin ++ show colWin) rowWin || colWin
+checkBoardWin b = rowWin || colWin
                     where
                           n = fst . snd $ bounds b
                           rowWin = (n+1) `elem` ([sum [b ! (i,j) | j <- [0..n]] | i <- [0..n]])
                           colWin = (n+1) `elem` ([sum [b ! (j,i) | j <- [0..n]] | i <- [0..n]])
-                          --diagLeftWin = (n+1) == sum [b ! (i,i) | i <- [0..n]]
-                          --diagRightWin = (n+1) == sum [b ! trace (show (i,n-i)) (i,n-i) | i <- [0..n]]
 
 checkWin :: [Board] -> Maybe Int
 checkWin boards = lookup True [(checkBoardWin (boards !! i), i) | i <- [0..length boards - 1]]
 
 computeAnswer :: Board -> Board -> Int -> Int
-computeAnswer board status n = trace (show $ [board ! i | (i,v) <- assocs status, v==0]) n*sum(zipWith (\x y -> -1*(y-1)*x) (elems board) (elems status))
+computeAnswer board status n = n*sum [board ! i | (i,v) <- assocs status, v==0]
 
 prettyPrint newStatus boards = intercalate "\n" [show row | row <- nested]
-                               where nested = [[( (newStatus !! 2) ! (j,i), (boards !! 2) ! (j,i)) | i <- [0..4]] | j<- [0..4]] 
+                               where nested = [[( (newStatus !! 2) ! (j,i), (boards !! 2) ! (j,i)) | i <- [0..4]] | j<- [0..4]]
 findWinner :: [Board] -- ^ bingo boards
   -> [Status] -- ^ bingo boards call status
   -> [Int] -- ^ list of called nums
   -> [ValueMap]
   -> Int -- solve1 answer
-findWinner boards boardStatus (x:xs) vmaps = case winner of (Just idx) -> trace ("winner " ++ show idx) computeAnswer (boards !! idx) (newStatus !! idx) x
+findWinner boards boardStatus (x:xs) vmaps = case winner of (Just idx) -> computeAnswer (boards !! idx) (newStatus !! idx) x
                                                             Nothing -> findWinner boards newStatus xs vmaps
                                                 where
                                                       newStatus = callNum boards boardStatus vmaps x
-                                                      winner = trace (show x ++ prettyPrint newStatus boards) checkWin newStatus
+                                                      winner = checkWin newStatus
 findWinner _ _ _ _ = error "uh oh"
