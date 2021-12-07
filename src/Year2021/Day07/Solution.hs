cf2e2170e93a6eb046c2ef9f0cbd1073090fb918
Christopher Wang <czw@mit.edu>
2021-12-07 00:38:17 -0500

    golf

--- src/Year2021/Day07/Solution.hs
+++ src/Year2021/Day07/Solution.hs
@@ -10,26 +10,23 @@ solve :: String -> IO()
 solve root = do
             print "hello"
             test <- readFile test_path
+            input1 <- readFile input1_path
             print $ show $ parseInput test
             print $ second ((==37).solve1) $ parseInput test
-            input1 <- readFile input1_path
             print $ second solve1 $ parseInput input1
-            --print $ second ((==26984457539).solve1 256) $ parseInput test
-            --print $ second (solve1 256) $ parseInput input1
+            print $ second ((==168).solve2) $ parseInput test
+            print $ second solve2 $ parseInput input1
           where
                 test_path = root ++ "Day07/test_input1.txt"
                 input1_path = root ++ "Day07/input1.txt"
 
-eol = char '\n'
-
 inputs :: GenParser Char st [Int]
-inputs = do digits <- sepBy (many1 digit) (char ',') <* many eol <* eof
+inputs = do digits <- sepBy (many1 digit) (char ',') <* many (char '\n') <* eof
             return $ map read digits
 
 parseInput = parse inputs "(unknown)"
 
-solve1 xs = err
-            where 
-                  absErr mean = sum [abs (compError x mean) | x <- xs]
-                  err = minimum $ map absErr [(minimum xs)..(maximum xs)]
-                  compError a b = sum [0..(max a b - min a b)]
+solve1 = solve12 (\x y -> abs (x-y))
+solve2 = solve12 (\x y -> sum [0..max x y-min x y])
+
+solve12 f_err xs = minimum $ map (sum . flip map xs . f_err) [(minimum xs)..(maximum xs)]
