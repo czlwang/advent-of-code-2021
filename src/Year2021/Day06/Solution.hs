2a7004cca1f7cca2e7ba4eadd8ac8d4383b21283
Christopher Wang <czw@mit.edu>
2021-12-06 23:18:02 -0500

    messy

--- src/Year2021/Day06/Solution.hs
+++ src/Year2021/Day06/Solution.hs
@@ -11,14 +11,10 @@ solve root = do
             test <- readFile test_path
             input1 <- readFile input1_path
             print $ parseInput test
-            print $ second (\x -> map (`solve1` x) [5..18]) $ parseInput test
-            --print $ second (solve1 14) $ parseInput test
-            --print $ solve1 6 [1]
-            --print $ solve1 7 [1]
-            --print $ solve1 8 [1]
-            --print $ solve1 9 [1]
-            --print $ second (solve1 256) $ parseInput input1
-            --print $ second solve12 $ parseInput input1
+            print $ second ((==5934).solve1 80) $ parseInput test
+            print $ second (solve1 80) $ parseInput input1
+            print $ second ((==26984457539).solve1 256) $ parseInput test
+            print $ second (solve1 256) $ parseInput input1
           where
                 test_path = root ++ "Day06/test_input1.txt"
                 input1_path = root ++ "Day06/input1.txt"
@@ -34,19 +30,21 @@ parseInput = parse day06 "(unknown)"
 
 solve1 :: Int -> [Int] -> Int
 --solve1 n squids = (length run - 1, run !! (length run - 1))-- sum [(run !! (n-o))*c | (o,c) <- offsetCounts]
-solve1 n squids = trace (show (zip [0..length run] run) ++ show offsetCounts) sum [(run !! (n-o-1))*c | (o,c) <- offsetCounts]
+--solve1 n squids = trace (show (zip [0..length run] run) ++ show offsetCounts) sum [(run !! (n-o-1))*c | (o,c) <- offsetCounts]
+--solve1 n squids = run !! n --sum [(run !! (n-o-1))*c | (o,c) <- offsetCounts]
+solve1 n squids = trace (show run) sum [(run !! (n-o-1))*c | (o,c) <- offsetCounts]
                     where
                         offsetCounts = map (\x-> (head x, length x)) $ group $ sort squids
-                        run = map (uncurry (+)) $ singleSolve n [(1,1)] [(1,1)]
+                        run = map (uncurry (+)) $ singleSolve n [1] [(1,1)]
 
 query ts t | t >=0     = ts !! t
-           | otherwise = (0,0)
+           | otherwise = 0
 
-singleSolve :: Int -> [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+singleSolve :: Int -> [Int] -> [(Int, Int)] -> [(Int, Int)]
 singleSolve n ts total | t == n+1 = total
-                       | otherwise = trace (show (new_mature, new_immature)) $ singleSolve n (ts ++ [(new_mature, new_immature)]) new_total
+                       | otherwise = singleSolve n (ts ++ [new_mature]) new_total
                     where t = length ts
                           (old_immature, old_mature) = last total
-                          new_mature   = snd $ query ts (t-9)
-                          new_immature = fst (query ts (t-7)) + new_mature
-                          new_total = total ++ [(new_mature + old_mature, new_immature + old_immature - new_mature)]
+                          -- who's giving birth this cycle? all the mature from 7 steps ago + all the immature from 9 steps ago
+                          new_mature = query ts (t-7) + query ts (t-9)
+                          new_total  = total ++ [(query ts (t-9) + old_mature, old_immature + query ts (t-7))]
