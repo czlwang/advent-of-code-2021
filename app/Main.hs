4b157fe01eba9791391bcb14f945369dcc63111b
Christopher Wang <czw@mit.edu>
2021-12-08 02:12:12 -0500

    day 08 solve

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@ import Year2021.Day04.Solution
 import Year2021.Day05.Solution
 import Year2021.Day06.Solution
 import Year2021.Day07.Solution
+import Year2021.Day08.Solution
 
 main :: IO ()
 main = do
@@ -22,6 +23,7 @@ main = do
             ("2021", "05") -> Year2021.Day05.Solution.solve root21 
             ("2021", "06") -> Year2021.Day06.Solution.solve root21 
             ("2021", "07") -> Year2021.Day07.Solution.solve root21 
+            ("2021", "08") -> Year2021.Day08.Solution.solve root21 
             (_, _) -> putStrLn "uhoh check day and year"
         where 
             root21 = "/home/czw/Documents/2021/aoc2021/src/Year2021/"
---
+++ src/Year2021/Day08/Solution.hs
@@ -0,0 +1,63 @@
+module Year2021.Day08.Solution (solve) where
+import Text.ParserCombinators.Parsec
+import Debug.Trace
+import Data.List
+import Data.Bifunctor
+import Control.Monad
+import Data.Array
+import Data.Maybe
+import qualified Data.Set as S
+
+solve :: String -> IO()
+solve root = do
+            test <- readFile test_path
+            print "hello"
+            input1 <- readFile input1_path
+            print $ second (show . last) $ parseInput test
+            print $ second ((==26).solve1) $ parseInput test
+            print $ second solve1 $ parseInput input1
+            print $ second ((==61229).solve2) $ parseInput test
+            print $ second solve2 $ parseInput input1
+          where
+                test_path = root ++ "Day08/test_input1.txt"
+                input1_path = root ++ "Day08/input1.txt"
+
+inLine :: GenParser Char st ([String], [String])
+inLine = do ins <- endBy1 (many1 letter) (char ' ')
+            string "| "
+            outs <- sepBy1 (many1 letter) (char ' ')
+            return (ins, outs)
+
+inputs :: GenParser Char st [([String], [String])]
+inputs = endBy inLine (void (char '\n') <|> eof)
+
+parseInput = parse inputs "(unknown)"
+
+solve1 :: [([String], [String])] -> Int
+solve1 inputs = length $ filter (`S.member` S.fromList [3,4,7,2]) $ map length $ concatMap snd inputs
+
+solve2 :: [([String], [String])] -> Int
+solve2 = sum . map (uncurry decode)
+
+takeOne xs = if length xs > 1 then error "uhoh" else head xs
+
+filterC = foldr filter
+
+decode :: [String] -> [String] -> Int
+decode ins outs = read $ concatMap show ints
+                where
+                unks  = map S.fromList ins
+                seven = takeOne $ filter ((==3).length) unks
+                one   = takeOne $ filter ((==2).length) unks
+                eight = takeOne $ filter ((==7).length) unks
+                four  = takeOne $ filter ((==4).length) unks
+                three = head $ filterC unks [(==2).length.S.intersection one, (==5).length]
+                nine  = S.union three four
+                zero  = takeOne $ filterC unks [(==6).length, (/= nine), (==2).length.S.intersection one]
+                six   = takeOne $ filterC unks [(==6).length, (/= zero), (/=nine)]
+                c     = S.difference zero six
+                two   = takeOne $ filterC unks [(==5).length, (/= three), (==1).length.S.intersection c]
+                five  = takeOne $ filterC unks [(==5).length, (/=three), (/=two)]
+                os    = map S.fromList outs
+                char2num nums o = fromJust $ elemIndex o nums
+                ints = map (char2num [zero, one, two, three, four, five, six, seven, eight, nine]) os
