a21de5c8e00f26538b7f8aa5e25e4230a99b6e74
Christopher Wang <czw@mit.edu>
2021-12-15 04:49:59 -0500

    dijkstra

--- src/Year2021/Day15/Solution.hs
+++ src/Year2021/Day15/Solution.hs
@@ -14,29 +14,17 @@ import Data.Maybe
 import qualified Data.Array as A
 import qualified Data.Set as S
 import qualified Data.Map as M
+import qualified Data.PQueue.Prio.Min as P
 
 solve :: String -> IO()
 solve root = do
             test <- readFile test_path
             test2 <- readFile test_path2
             input1 <- readFile input1_path
-            print $ parseInput test
-            let board1 = fromRight dummy $ parseInput test2
-            let board2 = (flip mkFullMap 5) $ fromRight dummy $ parseInput test
-            print(A.elems board1)
-            print(A.elems board2)
-            print (board1 == board2)
-            print $ second solve1 $ parseInput test
-            print $ second solve2 $ parseInput test
+            print $ second ((==40).solve1) $ parseInput test
+            print $ second ((==315).solve2) $ parseInput test
             print $ second solve1 $ parseInput input1
-            --print $ duplicate 5 10 ((0,0), 8)
-            --print $ second (flip mkFullMap 5) $ parseInput test
-            --print $ second solve2 $ parseInput test
-            --print $ second solve2 $ parseInput input1
-            --print $ second ((==1588).solve1 10) $ parseInput test
-            --print $ second (solve1 10) $ parseInput input1
-            --print $ second ((==2188189693529).solve1 40) $ parseInput test
-            --print $ second (solve1 40) $ parseInput input1
+            print $ second solve2 $ parseInput input1
           where
             test_path = root ++ "Day15/test_input1.txt"
             test_path2 = root ++ "Day15/test_input2.txt"
@@ -50,51 +38,56 @@ ruleParser = map digitToInt <$> many1 digit
 inputs :: GenParser Char st Board
 inputs = list2array <$> endBy ruleParser (void eol <|> eof)
 
-solve2 board = solve1 full
-            where
-            full = mkFullMap board 5
+solve1 = solve12 1
+solve2 = solve12 5
 
-solve1 board = result M.! end
+solve12 rep board = trace "call1" result M.! end
             where
-            end = snd $ A.bounds board
-            result = shortestRoute' board (M.fromList [((0,0),0)]) end
+            end = (newDim-1, newDim-1)
+            result = dkstra board (P.singleton (0,0) 0) initDist (M.fromList $ (,False) <$> idxs) (newDim*newDim) rep
+            maxDist = sum (A.elems board)*newDim^2
+            dim = (fst.snd) (A.bounds board) + 1
+            newDim = rep*dim
+            idxs = [(i,j) | i <- [0..newDim-1], j <- [0..newDim-1]]
+            initDist = M.fromList $ (,maxDist) <$> idxs
+
 
 duplicate n dim (coord, value) = nub res
                             where (x,y) = coord
-                                  res = [((x+i*dim, y+j*dim), v) | i <- [0..(n-1)], j<-[0..(n-1)], let v=(((value+i+j) `mod` 10) + ((value+i+j) `div` 10) `mod` 10)]
-
+                                  shiftVal i j = ((value+i+j) `mod` 10) + ((value+i+j) `div` 10) `mod` 10
+                                  res = [((x+i*dim, y+j*dim), v) | i <- [0..(n-1)], j<-[0..(n-1)], let v= shiftVal i j]
 
 mkFullMap :: Board -> Int -> Board
-mkFullMap b n = A.array ((0,0), (newDim-1, newDim-1)) (nub duplicates)
+mkFullMap b n =  A.array ((0,0), (newDim-1, newDim-1)) (nub duplicates)
             where
                 origs = A.assocs b
                 dim = (fst.snd) (A.bounds b) + 1
                 duplicates = concatMap (duplicate n dim) origs
                 newDim = n*dim
 
+getCoord n board (x,y) = shiftVal
+            where
+                dim = (fst.snd) (A.bounds board) + 1
+                origX = x `mod` dim
+                origY = y `mod` dim
+                i = x `div` dim
+                j = y `div` dim
+                value = board A.! (origX, origY)
+                shiftVal = ((value+i+j) `mod` 10) + ((value+i+j) `div` 10) `mod` 10
 
-queryCache :: M.Map Coord Int -> Coord -> Board -> M.Map Coord Int
-
-queryCache cache coord board | inCache = cache
-                             | otherwise = newCache
+dkstra :: Board -> P.MinPQueue Coord Int -> M.Map Coord Int -> M.Map Coord Bool -> Int -> Int -> M.Map Coord Int
+dkstra board queue dist visited nvisited rep | null queue = dist
+                                             | newNVisited==0 = dist
+                                             | otherwise = dkstra board newQueue newDist newVisited newNVisited rep
                             where
-                                inCache = coord `M.member` cache
-                                (m,n) = coord
-                                nbrs = filter (A.inRange (A.bounds board)) [(m+i, n+j) | (i,j) <- [(-1,0), (0,-1)]]
-                                minDistance = minimum $ map (cache M.!) nbrs
-                                newCache = M.insert coord (minDistance + (board A.! coord)) cache
-
-shortestRoute' :: Board -> M.Map Coord Int -> Coord -> M.Map Coord Int
-shortestRoute' board cached coord = foldl' (\cache (coord, val) -> queryCache cache coord board) cached (A.assocs board)
-
-shortestRoute :: Board -> M.Map Coord Int -> Coord -> M.Map Coord Int
-shortestRoute board cached coord | inCache = cached
-                                 | otherwise = newCache'''
-                                    where
-                                        inCache = coord `M.member` cached
-                                        (m,n) = coord
-                                        nbrs = filter (A.inRange (A.bounds board)) [(m+i, n+j) | (i,j) <- [(-1,0), (0,-1)]]
-                                        --nbrs = getArrayNeighbors coord board
-                                        newCache = foldl (shortestRoute board) cached nbrs
-                                        minDistance = minimum $ map (newCache M.!) nbrs
-                                        newCache''' = M.insert coord (minDistance + (board A.! coord)) newCache
+                                ((coord, val), q') = P.deleteFindMin queue
+                                gCoord = getCoord 1 board
+                                getNbrs (m,n) = filter (A.inRange ((0,0),(newDim,newDim))) [(m+i, n+j) | (i,j) <- [(0,1), (1,0), (-1,0), (0,-1)]]
+                                dim = (fst.snd) (A.bounds board) + 1
+                                newDim = rep*dim - 1
+                                newNVisited = if not(visited M.! coord) then nvisited - 1 else nvisited
+                                nbrs = filter (not.(visited M.!)) $ getNbrs coord
+                                alt = [(n,newD) | n <- nbrs, let d = dist M.! n, let newD = gCoord n + val, newD<d]
+                                newDist = foldl' (\acc (x,y) -> M.insert x y acc) dist alt
+                                newQueue = foldl' (\acc (x,y) -> P.insert x y acc) q' alt
+                                newVisited = M.insert coord True visited
