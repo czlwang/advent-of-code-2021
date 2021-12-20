77ce3715c96f282fdb0fa1238a133439b1d4fe18
Christopher Wang <czw@mit.edu>
2021-12-20 15:52:36 -0600

    if/else explode

--- src/Year2021/Day18/Solution.hs
+++ src/Year2021/Day18/Solution.hs
@@ -24,7 +24,7 @@ solve root = do
             input1_path = root ++ "Day18/input1.txt"
             parseInput = parse inputs "(unknown)"
 
-data Snail = Pair Snail Snail | Regular Int deriving Show
+data Snail = Pair {snailL :: Snail, snailR :: Snail} | Regular Int deriving Show
 
 snailPair :: GenParser Char st Snail
 snailPair = do
@@ -42,3 +42,59 @@ snailNum = between (char '[') (char ']') snailPair
 inputs :: GenParser Char st [Snail]
 inputs = endBy snailNum (void eol <|> eof)
 
+modifyLeft :: Int -> Snail -> (Snail, Bool)
+modifyLeft n root = case root of 
+                        (Regular x) -> (Regular (x+n), True)
+                        (Pair left right) -> if mleft
+                                             then (Pair right newLeft, mleft)
+                                             else if mright
+                                             then (Pair newRight left, mright)
+                                             else (root, False)
+                                             where 
+                                                  recurse = modifyLeft n
+                                                  (newLeft, mleft) = recurse left
+                                                  (newRight, mright) = recurse right
+                                                    
+
+modifyRight :: Int -> Snail -> (Snail, Bool)
+modifyRight n root = case root of 
+                        (Regular x) -> (Regular (x+n), True)
+                        (Pair left right) -> if mright
+                                             then (Pair newRight left, mright)
+                                             else if mleft
+                                             then (Pair right newLeft, mleft)
+                                             else (root, False)
+                                             where 
+                                                  recurse = modifyLeft n
+                                                  (newLeft, mleft) = recurse left
+                                                  (newRight, mright) = recurse right
+
+getRight :: Snail -> Int
+getRight (Pair (Regular x) (Regular _)) = x
+getRight _ = error "uhoh"
+
+getLeft :: Snail -> Int
+getLeft (Pair (Regular _) (Regular x)) = x
+getLeft _ = error "uhoh"
+
+explode :: Snail -> Int -> (Bool, Bool, Bool, Snail, Snail)
+explode root depthCount
+                        | depthCount<4 = if explodedLeft then
+                                            if elRight 
+                                            then (explodedLeft, elLeft, newElRight, Pair newRight elResult, exlPair)
+                                            else (explodedLeft, elLeft, elRight, noChange, exlPair)
+                                         else if explodedRight then
+                                           if erLeft 
+                                           then (explodedRight, erLeft, newErLeft, Pair erResult newLeft, exrPair)
+                                           else (explodedRight, erLeft, erRight, noChange, exrPair)
+                                         else (False, False, False, noChange, dummyPair)
+                        | depthCount==4 = (True, False, False, root, root)
+                        where 
+                            (left,right) = (snailL root, snailR root)
+                            (explodedLeft, elLeft, elRight, elResult, exlPair) = explode left (depthCount +1)
+                            (newRight, newElRight) = modifyLeft (getRight exlPair) right
+                            (explodedRight, erLeft, erRight, erResult, exrPair) = explode right (depthCount +1)
+                            (newLeft, newErLeft) = modifyLeft (getLeft exrPair) left
+                            noChange = root
+                            dummyPair = Pair (Regular 1) (Regular 1)
+explode root _ = error "uhoh"
