de20218282d03ea92370f8d8c34b2ce1c881f31e
Christopher Wang <czw@mit.edu>
2021-12-21 14:56:32 -0600

    cleaner

--- src/Year2021/Day18/Solution.hs
+++ src/Year2021/Day18/Solution.hs
@@ -1,54 +1,39 @@
 {-# LANGUAGE RecordWildCards #-}
 module Year2021.Day18.Solution (solve) where
-import Debug.Trace
 import Lib
 import Text.ParserCombinators.Parsec
 import Data.Bifunctor
 import Control.Monad
-import Data.Char
-import Data.Either
 import qualified Data.Map as M
 import qualified Data.Set as S
 
 solve :: String -> IO()
 solve root = do
             test <- readFile test_path
-            test2 <- readFile test_path2
             input1 <- readFile input1_path
-            print $ parseInput test
-            print "hello"
-            print $ testF explode "[[[[[9,8],1],2],3],4]" "[[[[0,9],2],3],4]"
-            print $ testF explode "[7,[6,[5,[4,[3,2]]]]]" "[7,[6,[5,[7,0]]]]"
-            print $ testF explode "[[6,[5,[4,[3,2]]]],1]" "[[6,[5,[7,0]]],3]"
-            print $ testF explode "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-            print $ testF explode "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
-            print $ testF split "[[[[0,7],4],[15,[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
-            print $ testF split "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
-            print $ testF split "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
             print $ second ((==4140).solve1) $ parseInput test
             print $ second solve1 $ parseInput input1
-            print $ second ((==143).magnitude.head) $ parseInput "[[1,2],[[3,4],5]]"
-            print $ testAdd "[[[[4,3],4],4],[7,[[8,4],9]]]" "[1,1]" "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
-            print $ second solve2 $ parseInput input1
-            print $ second solve2 $ parseInput test
             print $ second ((==3993).solve2) $ parseInput test
-            --print $ second ((==45).solve1) $ parseInput test
-            --print $ second ((==112).solve2) $ parseInput test
-            --print $ second solve2 $ parseInput input1
+            print $ second solve2 $ parseInput input1
           where
             test_path = root ++ "Day18/test_input1.txt"
             test_path2 = root ++ "Day18/test_input2.txt"
             input1_path = root ++ "Day18/input1.txt"
             parseInput = parse inputs "(unknown)"
-            testF f s1 s2 = parseInput s2 == second (map f) (parseInput s1)
-            dummy = Regular 1
-            unwrap s = head $ fromRight [dummy] $ parseInput s
-            testAdd s1 s2 s3 = unwrap s3 == sreduce (add (unwrap s1) (unwrap s2))
 
 data Snail = Pair {snailL :: Snail, snailR :: Snail} | Regular Int deriving (Show, Eq)
 
+snailRegular :: GenParser Char st Snail
+snailRegular = Regular . read <$> many1 digit
+
+snailNum :: GenParser Char st Snail
+snailNum = between (char '[') (char ']') snailPair
+
+inputs :: GenParser Char st [Snail]
+inputs = endBy snailNum (void eol <|> eof)
+
 solve1 :: [Snail] -> Int
-solve1 ss = magnitude $ foldl1 (\acc s -> sreduce (add acc s)) ss
+solve1 = magnitude . foldl1 (\acc s -> sreduce (add acc s))
 
 solve2 :: [Snail] -> Int
 solve2 ss = maximum $ magnitude.sreduce <$> allPairs
@@ -60,20 +45,10 @@ solve2 ss = maximum $ magnitude.sreduce <$> allPairs
 magnitude (Regular x) = x
 magnitude (Pair left right) = 3*magnitude left + 2*magnitude right
 snailPair :: GenParser Char st Snail
-snailPair = do
-            left <- try snailRegular <|> snailNum
-            char ','
-            right <- try snailRegular <|> snailNum
-            return $ Pair left right
-
-snailRegular :: GenParser Char st Snail
-snailRegular = Regular . read <$> many1 digit
-
-snailNum :: GenParser Char st Snail
-snailNum = between (char '[') (char ']') snailPair
-
-inputs :: GenParser Char st [Snail]
-inputs = endBy snailNum (void eol <|> eof)
+snailPair = do left <- try snailRegular <|> snailNum
+               char ','
+               right <- try snailRegular <|> snailNum
+               return $ Pair left right
 
 modifyLeftmost :: Bool -> Int -> Snail -> (Snail, Bool)
 modifyLeftmost False _ root = (root, False)
@@ -108,7 +83,9 @@ explode s = exploded
             where
             (_,_,_,exploded,_) = explode' s 0
 
-explode' :: Snail -> Int -> (Bool, Bool, Bool, Snail, Snail)
+explode' :: Snail -- ^ check for explode in this snail
+  -> Int -- ^ depth of recursion
+  -> (Bool, Bool, Bool, Snail, Snail) -- ^ (did this snail explode?, have we modified the rightmost?, the leftmost?, what's the resulting snail?, what's the exploded snail?)
 explode' root@(Regular x) depthCount = (False, False, False, root, Pair (Regular 1) (Regular 1))
 explode' root depthCount
                          | depthCount<4 = if explodedLeft then
