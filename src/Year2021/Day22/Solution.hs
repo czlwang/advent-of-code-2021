1f16b96b7e25f4f586730b9f16554532a3e352c0
Christopher Wang <czw@mit.edu>
2021-12-26 22:57:00 -0600

    solve2

--- src/Year2021/Day22/Solution.hs
+++ src/Year2021/Day22/Solution.hs
@@ -17,18 +17,15 @@ import Data.Char
 solve :: String -> IO()
 solve root = do
             test <- readFile test_path
-            test2 <- readFile test_path2
+            test3 <- readFile test_path3
             input1 <- readFile input1_path
-            --print $ parseInput test
-            print $ second solve1 $ parseInput test2
-            print $ second solve1 $ parseInput test
-            --print $ second ((==739785).solve1) $ parseInput test
-            --print $ second solve1 $ parseInput input1
-            --print $ second ((==444356092776315).solve2) $ parseInput test
-            --print $ second solve2 $ parseInput input1
+            print $ second ((==590784).solve1) $ parseInput test
+            print $ second solve1 $ parseInput input1
+            print $ second ((==2758514936282235).solve2) $ parseInput test3
+            print $ second solve2 $ parseInput input1
           where
             test_path = root ++ "Day22/test_input1.txt"
-            test_path2 = root ++ "Day22/test_input2.txt"
+            test_path3 = root ++ "Day22/test_input3.txt"
             input1_path = root ++ "Day22/input1.txt"
             parseInput = parse inputs "(unknown)"
 
@@ -62,6 +59,8 @@ solve1 toggles = getMagnitude $ processToggles [] filtered
                     inBounds (x1,x2) = abs x1 <= 50 || abs x2 <= 50
                     filtered = filter (all inBounds.head.snd) toggles
 
+solve2 = getMagnitude . processToggles []
+
 findBoundIntersection :: Bounds -> Bounds -> Maybe Bounds
 findBoundIntersection (a1,a2) (b1, b2) | intersect = Just (lower, upper)
                                        | otherwise = Nothing
@@ -91,10 +90,11 @@ splitCuboid a b = case intersects of Nothing -> Nothing
                     where
                         intersects = findCuboidIntersection a b
 
-toggleCuboid :: Cuboid -> (Bool, [Cuboid]) -> [Cuboid] -> Maybe ([Cuboid], [Cuboid]) -- check o with a list of toggles. if it intersects with any of them, return the updated toggles and cuboid split
+-- check o with a list of toggles. if it intersects with any of them, return the updated toggles and cuboid split
+toggleCuboid :: Cuboid -> (Bool, [Cuboid]) -> [Cuboid] -> Maybe ([Cuboid], [Cuboid])
 toggleCuboid cuboid (on, []) past = Nothing
 toggleCuboid cuboid (on, t:ts) past = case split of Nothing -> toggleCuboid cuboid (on, ts) (t:past)
-                                                    Just (intersection, newCuboid, newToggle) -> trace ("intersection " ++ show intersection) Just (if on
+                                                    Just (intersection, newCuboid, newToggle) -> Just (if on
                                                                                                        then
                                                                                                         (intersection:newCuboid,
                                                                                                          concat [newToggle, ts, past])
@@ -108,13 +108,13 @@ toggleCuboids :: [Cuboid] -> (Bool, [Cuboid]) -> [Cuboid] -> [Cuboid]
 toggleCuboids [] (on, togglers) processed | on = processed ++ togglers
                                           | otherwise = processed
 toggleCuboids ons@(o:os) (on, togglers) processed = case toggled of Nothing -> toggleCuboids os (on, togglers) (o:processed)
-                                                                    Just (newOns, newToggle) -> trace ("toggled cuboid " ++ show newOns ++ " new toggles " ++ show newToggle) toggleCuboids (newOns++os) (on, newToggle) processed
-                                    where
-                                        toggled = trace ("toggleCuboid " ++ show o ++ "togglers " ++ show togglers) toggleCuboid o (on, togglers) []
+                                                                    Just (newOns, newToggle) -> toggleCuboids (newOns++os) (on, newToggle) processed
+                                                        where
+                                                            toggled = toggleCuboid o (on, togglers) []
 
 processToggles :: [Cuboid] -> [(Bool, [Cuboid])] -> [Cuboid]
 processToggles ons [] = ons
-processToggles ons (t:ts) = trace ("processToggles " ++ show ons ++ " toggle " ++ show t) processToggles toggled ts
+processToggles ons (t:ts) = processToggles toggled ts
                             where toggled = toggleCuboids ons t []
 
 getMagnitude :: [Cuboid] -> Int
